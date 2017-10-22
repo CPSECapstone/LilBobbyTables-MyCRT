15932a8e2a2ac273b4649c49092bb3df15f53d9f
Cameron Taylor <axis7818@gmail.com>
2017-10-22 15:19:12 -0700

    index.html

--- service/main.js
+++ service/main.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rest API Routes
 app.use('/capture', require('./routes/capture'));
 app.use('/replay', require('./routes/replay'));
 
+// home page
 app.get('/', (request, response) => {
-   response.send('Hello MyCRT');
+   response.sendfile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(3000);
+// start the server
+var port = process.env.NODE_PORT || 3000;
+var host = process.env.NODE_IP || 'localhost';
+app.listen(port, host, () => {
+   console.log("App Listening on " + host + ":" + port);
+});
