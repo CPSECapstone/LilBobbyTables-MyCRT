41cb95568bc63e702cde6bfe18695314821a007a
Cameron Taylor <axis7818@gmail.com>
2017-10-23 21:13:50 -0700

    test typescript for the replay package

--- replay/src/main.ts
+++ replay/src/main.ts
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
 console.log("MyCRT Replay Program");
+
+const testVal = 42;
+
+export { testVal }
---
+++ service/test.js
@@ -0,0 +1,3 @@
+var replay = require('@lbt-mycrt/replay');
+
+console.log(replay.testVal);
