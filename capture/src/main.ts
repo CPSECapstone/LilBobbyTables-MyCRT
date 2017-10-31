478debb004fca4202bd0a2607abae7150906f170
Cameron Taylor <axis7818@gmail.com>
2017-10-31 12:18:16 -0700

    fix capture launching

--- capture/src/main.ts
+++ capture/src/main.ts
@@ -2,9 +2,7 @@
 
 import launch from './launch';
 
-export default {
-   launch,
-};
+export default launch;
 
 if (typeof(require) !== 'undefined' && require.main === module) {
    console.log("Running MyCRT Capture Program");
