a50bfcd190fa6fa7c7eb37731c02eff6d2cb5467
Christiana Ushana <cushana@calpoly.edu>
2017-11-09 09:31:05 -0800

    added ci_script

---
+++ scripts/ci_script.sh
@@ -0,0 +1,23 @@
+# setup for scripts directory
+npm install
+cd ..
+# setup for common directory
+cd common
+npm install
+cd ..
+# setup for capture directory
+cd capture
+npm install
+cd ..
+# setup for replay directory
+cd replay
+npm install
+cd ..
+# setup for service directory
+cd service
+npm install
+cd ..
+# setup for cli directory
+cd cli
+npm install
+cd ..
