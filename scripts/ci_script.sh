c6d703ca2a0d6c06c0909d03befa8cf6726c9c63
Cameron Taylor <axis7818@gmail.com>
2017-11-14 21:58:16 -0800

    adjust ci_script to fail correctly

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -1,41 +1,40 @@
+#!/usr/bin/env bash
+
+set -e # fail when any single command fails
+
+# set up some variables
+
 ROOT_DIR="$(pwd)"
 SCRIPTS_DIR="${ROOT_DIR}/scripts"
 COMMON_DIR="${ROOT_DIR}/common"
 CAPTURE_DIR="${ROOT_DIR}/capture"
 REPLAY_DIR="${ROOT_DIR}/replay"
 SERVICE_DIR="${ROOT_DIR}/service"
-CLI_DIR=="${ROOT_DIR}/cli"
+CLI_DIR="${ROOT_DIR}/cli"
+
+for mod_dir in $ROOT_DIR $SCRIPTS_DIR $COMMON_DIR $CAPTURE_DIR $REPLAY_DIR $SERVICE_DIR $CLI_DIR; do
+   if [ ! -d "$mod_dir" ]; then
+      echo "Could not find directory for the $(basename $mod_dir) module."
+      echo "Make sure this script is being executed from the root of the repository"
+      exit 1
+   fi
+done
 
 # setup for scripts directory
 cd $SCRIPTS_DIR
 npm install
 
-# setup for common directory
-cd $COMMON_DIR
-npm install --production
-cd $SCRIPTS_DIR
-npm run build-common
+# install and build all modules
+install_and_build() {
+   cd $1
+   npm install --production
+   cd $SCRIPTS_DIR
+   npm run $2
+}
 
-# setup for capture directory
-cd $CAPTURE_DIR
-npm install --production
-cd $SCRIPTS_DIR
-npm run build-capture
+install_and_build $COMMON_DIR build-common
+install_and_build $CAPTURE_DIR build-capture
+install_and_build $REPLAY_DIR build-replay
+install_and_build $SERVICE_DIR build-service
+install_and_build $CLI_DIR build-cli
 
-# setup for replay directory
-cd $REPLAY_DIR
-npm install --production
-cd $SCRIPTS_DIR
-npm run build-replay
-
-# setup for service directory
-cd $SERVICE_DIR
-npm install --production
-cd $SCRIPTS_DIR
-npm run build-service
-
-# setup for cli directory
-cd $CLI_DIR
-npm install --production
-cd $SCRIPTS_DIR
-npm run build-cli
