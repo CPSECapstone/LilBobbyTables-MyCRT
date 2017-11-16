b0a904802401187487fe4e3c25ccae67664ac82e
Cameron Taylor <axis7818@gmail.com>
2017-11-16 00:02:43 -0800

    server test

--- service/src/test/main.test.ts
+++ service/src/test/main.test.ts
@@ -1,5 +1,6 @@
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
+import { Server } from 'http';
 import * as http from 'http-status-codes';
 import 'mocha';
 
@@ -10,7 +11,31 @@ process.env.NODE_ENV = 'test';
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe("MyCRTService index", () => {
+describe("MyCRTService", () => {
+   let mycrt: MyCRTService;
+
+   beforeEach(() => {
+      mycrt = new MyCRTService();
+   });
+
+   afterEach(() => {
+      mycrt.close();
+   });
+
+   it("should launch without crashing", () => {
+      mycrt.launch();
+   });
+
+   it("should have a Server instance after launching, but not after closing", () => {
+      mycrt.launch();
+      expect(mycrt.getServer()).to.be.instanceOf(Server);
+      mycrt.close();
+      const testNull = expect(mycrt.getServer()).to.be.null;
+   });
+
+});
+
+describe("MyCRTService GET /", () => {
 
    let mycrt: MyCRTService;
    before(() => {
