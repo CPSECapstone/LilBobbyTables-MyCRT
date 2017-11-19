9fbefb1e10737314479d321c58b6ca5e49fe039d
Nishanth <nishanth.dara@gmail.com>
2017-11-19 15:17:34 -0800

    Added non-AWS related DB setup

--- service/db/bootstrap.sql
+++ service/db/bootstrap.sql
@@ -1,11 +1,12 @@
-# TODO: populate this
-
 DROP DATABASE IF EXISTS LBTMyCRT;
 CREATE DATABASE LBTMyCRT;
 USE LBTMyCRT;
 
 CREATE TABLE Environment (
-   id int(11) AUTO_INCREMENT PRIMARY KEY
+   id int(11) AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32),
+   dbId INT(11) REFERENCES DBReference(id),
+   s3id INT(11) REFERENCES S3Reference(id)
 );
 
 CREATE TABLE DBReference (
@@ -21,9 +22,17 @@ CREATE TABLE Metrics (
 );
 
 CREATE TABLE Capture (
-   id int(11) AUTO_INCREMENT PRIMARY KEY
+   id int(11) AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32),
+   start DATETIME,
+   end DATETIME,
+   envId INT(11) REFERENCES Environment(id)
 );
 
 CREATE TABLE Replay (
-   id int(11) AUTO_INCREMENT PRIMARY KEY
+   id int(11) AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32),
+   start DATETIME,
+   end DATETIME,
+   capId INT(11) REFERENCES Capture(id)
 );
