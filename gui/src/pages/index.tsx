091ed35aa91b36cde0c75470f5972044c0343424
Hilary Schulz <hils124@yahoo.com>
2017-11-24 23:13:36 -0800

    Made capture button and text field for results

--- gui/src/pages/index.tsx
+++ gui/src/pages/index.tsx
@@ -3,20 +3,41 @@ import '../../static/css/index.css';
 import React = require('react');
 import ReactDom = require('react-dom');
 
-const IndexApp = () => {
+const Title = () => {
    return (
-      <div className="index-style-test">
-         <p>Hello from index.tsx</p>
-
-         <ul>
-            <li>and</li>
-            <li>this</li>
-            <li>is</li>
-            <li>a</li>
-            <li>list</li>
-         </ul>
+      <div className="title-block">
+          <h1 className="my-crt-title">Capture a Workload</h1>
+          <h2 className="amazon-subtitle">Amazon Web Services</h2>
       </div>
    );
 };
 
-ReactDom.render(<IndexApp />, document.getElementById('index-app'));
+class ButtonComponent extends React.Component<any, any> {
+        constructor(props: any) {
+            super(props);
+            this.state = { name: "Start", active: false };
+        }
+        public handleChange(event: any): void {
+            this.setState({ active: !this.state.active });
+        }
+        public render() {
+            return (
+                <div>
+                    <p className="capture-name">Lil-Bobby-Tables-Capture</p>
+                    <button className={this.state.active ? 'stop-capture' : 'start-capture'}
+                            onClick = { (e) => this.handleChange(e) }
+                    >{ this.state.active ? 'Stop' : 'Start'}</button>
+                </div>
+            );
+        }
+}
+
+const TextField = () => {
+    return (
+        <textarea className="result-textfield"></textarea>
+    );
+};
+
+ReactDom.render(<Title />, document.getElementById('title-block'));
+ReactDom.render(<ButtonComponent />, document.getElementById('capture-button'));
+ReactDom.render(<TextField />, document.getElementById('capture-results'));
