cd4963c8ac387812d32e8b7991448b0d7a56c261
Nishanth <nishanth.dara@gmail.com>
2017-12-25 13:45:23 -0800

    Enabled ping router to get captures

--- service/src/routes/ping.ts
+++ service/src/routes/ping.ts
@@ -1,6 +1,8 @@
+import { Logging } from '@lbt-mycrt/common';
 import http = require('http-status-codes');
-
+import * as mysql from 'mysql';
 import SelfAwareRouter from './self-aware-router';
+import ConnectionPool from './util/cnnPool';
 
 export default class PingRouter extends SelfAwareRouter {
 
@@ -8,9 +10,13 @@ export default class PingRouter extends SelfAwareRouter {
    public urlPrefix: string = '/ping';
 
    protected mountRoutes(): void {
+      const logger = Logging.defaultLogger(__dirname);
 
       this.router.get('/', (request, response) => {
-         response.status(http.OK).end();
+         const queryStr = mysql.format("SELECT * FROM Capture", []);
+         ConnectionPool.query(response, queryStr, (error, rows, fields) => {
+            response.json(rows);
+         });
       });
 
    }
