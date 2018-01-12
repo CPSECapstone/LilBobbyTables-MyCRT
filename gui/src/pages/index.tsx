bfe7f52d84f045d8838645bcdadf98553a17bd4b
Hilary Schulz <hils124@yahoo.com>
2018-01-12 10:42:52 -0800

    Made bootstrap nav bar and installed recharts

--- gui/src/pages/index.tsx
+++ gui/src/pages/index.tsx
@@ -1,59 +1,48 @@
+import './common';
+
 import '../../static/css/index.css';
 
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { BrowserLogger as logger } from '../logging';
 
 import React = require('react');
 import ReactDom = require('react-dom');
 
-import './common';
 import { mycrt } from './utils/mycrt-client'; // client for interacting with the service
 
-const Title = () => {
-   return (
-      <div className="title-block">
-          <h1 className="my-crt-title">Capture a Workload</h1>
-          <h2 className="amazon-subtitle">Amazon Web Services</h2>
-      </div>
-   );
-};
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2291},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
 
-class ButtonComponent extends React.Component<any, any> {
-        constructor(props: any) {
-            super(props);
-            this.state = { name: "Start", active: false, id: null, result: null };
-        }
-        public async handleChange(event: any) {
-            this.setState({ active: !this.state.active });
-            if (!this.state.active) {
-                const captureId = await mycrt.postCapture({ end: null, name: 'lbt-capture',
-                    start: "2017-11-02 12:00:00" });
-                logger.info(`Got capture id: ${captureId}`);
-                if (captureId) {
-                    this.setState({ id: captureId });
-                }
-            } else {
-                let result = await mycrt.stopCapture(this.state.id);
-                logger.info(`${result}`);
-                if (!result) {
-                    result = `Capture ${this.state.id}: Failed to get capture result.`;
-                }
-                this.setState({ result });
-            }
-        }
-        public render() {
-            return (
-                <div>
-                    <p className="capture-name">Lil-Bobby-Tables-Capture</p>
-                    <button className={this.state.active ? 'stop-capture' : 'start-capture'}
-                            onClick = { (e) => this.handleChange(e) }
-                    >{ this.state.active ? 'Stop' : 'Start'}</button>
-                    <br/>
-                    <textarea className="result-textfield"
-                        value={this.state.result ? JSON.stringify(this.state.result, null, "\t") : ''}></textarea>
-                </div>
-            );
-        }
-}
+// const data2 = [
+//       {name: 'Page A', uv: 4000, pv: 1000, amt: 2400},
+//       {name: 'Page B', uv: 3000, pv: 1333, amt: 2210},
+//       {name: 'Page C', uv: 2000, pv: 2222, amt: 2291},
+//       {name: 'Page D', uv: 2780, pv: 3333, amt: 2000},
+//       {name: 'Page E', uv: 1890, pv: 2222, amt: 2181},
+//       {name: 'Page F', uv: 2390, pv: 4444, amt: 2500},
+//       {name: 'Page G', uv: 3490, pv: 3332, amt: 2100},
+// ];
 
-ReactDom.render(<Title />, document.getElementById('title-block'));
-ReactDom.render(<ButtonComponent />, document.getElementById('capture-button'));
+const IndexApp = () => {
+    return (
+        <LineChart width={800} height={500} data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" strokeDasharray="3 3" />
+        </LineChart>
+    );
+ };
+
+ReactDom.render(<IndexApp />, document.getElementById('index-app'));
