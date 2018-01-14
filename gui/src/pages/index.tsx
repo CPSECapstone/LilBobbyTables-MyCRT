e7867db243cea221767ceaebe66f24a105d37e64
Hilary Schulz <hils124@yahoo.com>
2018-01-13 19:13:46 -0800

    Separated metrics and started making flow through each page of app

--- gui/src/pages/index.tsx
+++ gui/src/pages/index.tsx
@@ -6,46 +6,17 @@ import { BrowserLogger as logger } from '../logging';
 
 import React = require('react');
 import ReactDom = require('react-dom');
-import { Graph } from './components/graph_comp';
 
 import { mycrt } from './utils/mycrt-client'; // client for interacting with the service
 
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2291},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
-const data2 = [
-      {name: 'Page A', uv: 4000, pv: 1000, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1333, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 2222, amt: 2291},
-      {name: 'Page D', uv: 2780, pv: 3333, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 2222, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 4444, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 3332, amt: 2100},
-];
-
-const data3 = [
-    {name: 'Page A', uv: 4000, pv: 1333, amt: 2400},
-    {name: 'Page B', uv: 3000, pv: 2400, amt: 2210},
-    {name: 'Page C', uv: 2000, pv: 2222, amt: 2291},
-    {name: 'Page D', uv: 2780, pv: 4444, amt: 2000},
-    {name: 'Page E', uv: 1890, pv: 3800, amt: 2181},
-    {name: 'Page F', uv: 2390, pv: 4444, amt: 2500},
-    {name: 'Page G', uv: 3490, pv: 1000, amt: 2100},
-];
-
 const IndexApp = () => {
     return (
-        <div>
-            <Graph title={"CPU Load Chart"} data={data}/>
-            <Graph title={"Memory Chart"} data={data2}/>
-            <Graph title={"I/O Chart"} data={data3} />
+        <div className="jumbotron">
+            <div className="container">
+                <h1>MyCRT</h1>
+                <p>Amazon Web Services</p>
+                <p><a className="btn btn-primary btn-lg" href="/metrics" role="button">Get Started</a></p>
+            </div>
         </div>
     );
  };
--- gui/src/pages/metrics.tsx
+++ gui/src/pages/metrics.tsx
@@ -1,22 +1,51 @@
+import './common';
+
 import '../../static/css/metrics.css';
 
 import React = require('react');
 import ReactDom = require('react-dom');
 
-const MetricsApp = () => {
-   return (
-      <div className="metrics-style-test">
-         <p>Hello from metrics.tsx</p>
+import { Graph } from './components/graph_comp';
 
-         <ul>
-            <li>and</li>
-            <li>this</li>
-            <li>is</li>
-            <li>a</li>
-            <li>list</li>
-         </ul>
-      </div>
-   );
+const data = [
+    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+    {name: 'Page C', uv: 2000, pv: 9800, amt: 2291},
+    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
+const data2 = [
+    {name: 'Page A', uv: 4000, pv: 1000, amt: 2400},
+    {name: 'Page B', uv: 3000, pv: 1333, amt: 2210},
+    {name: 'Page C', uv: 2000, pv: 2222, amt: 2291},
+    {name: 'Page D', uv: 2780, pv: 3333, amt: 2000},
+    {name: 'Page E', uv: 1890, pv: 2222, amt: 2181},
+    {name: 'Page F', uv: 2390, pv: 4444, amt: 2500},
+    {name: 'Page G', uv: 3490, pv: 3332, amt: 2100},
+];
+
+const data3 = [
+  {name: 'Page A', uv: 4000, pv: 1333, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 2400, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 2222, amt: 2291},
+  {name: 'Page D', uv: 2780, pv: 4444, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 3800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 4444, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 1000, amt: 2100},
+];
+
+const MetricsApp = () => {
+    return (
+        <div>
+            <h1 style={{"text-align": "center"}}>Capture Metrics</h1>
+            <Graph title={"CPU Load Chart"} data={data} />
+            <Graph title={"Memory Chart"} data={data2} />
+            <Graph title={"I/O Chart"} data={data3} />
+        </div>
+    );
 };
 
 ReactDom.render(<MetricsApp />, document.getElementById('metrics-app'));
