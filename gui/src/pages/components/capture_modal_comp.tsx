9febfb192c0dc4625f920b3172c64e4e1a04a072
Hilary Schulz <hils124@yahoo.com>
2018-01-30 18:18:44 -0800

    Added more changes to capture pop-up modal and styling

--- gui/src/pages/components/capture_modal_comp.tsx
+++ gui/src/pages/components/capture_modal_comp.tsx
@@ -1,6 +1,8 @@
 import React = require('react');
 import ReactDom = require('react-dom');
 
+import { StartDateTime } from './start_date_time_comp';
+
 export class CaptureModal extends React.Component<any, any>  {
 
     public render() {
@@ -8,27 +10,31 @@ export class CaptureModal extends React.Component<any, any>  {
             <div className="modal fade" id={this.props.id} role="dialog"
                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
+                    <div className="modal-content myCRT-modal">
+                        <div className="modal-header myCRT-modal-header">
                             <h5 className="modal-title">New Capture</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+                                <span aria-hidden="true" style={{color: "white"}}>&times;</span>
                             </button>
                         </div>
                         <div className="modal-body">
                             <form>
                                 <div className="form-group">
-                                    <label>Capture Name</label>
+                                    <label><b>Capture Name</b></label>
                                     <input type="name" className="form-control" id="captureName"
                                            aria-describedby="captureName" placeholder="Enter name"></input>
-                                        <small id="captureName" className="form-text text-muted"></small>
+                                    <small id="captureName" className="form-text text-muted"></small>
+                                    <br/>
+                                    <StartDateTime />
+                                    <br/>
+                                    <label><b>Duration</b></label>
                                 </div>
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary"
                                 data-dismiss="modal">Cancel</button>
-                            <button type="button" className="btn btn-primary">Save Changes</button>
+                            <button type="button" className="btn btn-info">Save Changes</button>
                         </div>
                     </div>
                 </div>
--- gui/src/pages/components/replay_modal_comp.tsx
+++ gui/src/pages/components/replay_modal_comp.tsx
@@ -8,17 +8,17 @@ export class ReplayModal extends React.Component<any, any>  {
             <div className="modal fade" id={this.props.id} role="dialog"
                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
+                    <div className="modal-content myCRT-modal">
+                        <div className="modal-header myCRT-modal-header">
                             <h5 className="modal-title">New Replay</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+                                <span aria-hidden="true" style={{color: "white"}}>&times;</span>
                             </button>
                         </div>
                         <div className="modal-body">
                             <form>
                                 <div className="form-group">
-                                    <label>Replay Name</label>
+                                    <label><b>Replay Name</b></label>
                                     <input type="name" className="form-control" id="replayName"
                                            aria-describedby="replayName" placeholder="Enter name"></input>
                                         <small id="replayName" className="form-text text-muted"></small>
@@ -27,7 +27,7 @@ export class ReplayModal extends React.Component<any, any>  {
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <button type="button" className="btn btn-primary">Save Changes</button>
+                            <button type="button" className="btn btn-info">Save Changes</button>
                         </div>
                     </div>
                 </div>
