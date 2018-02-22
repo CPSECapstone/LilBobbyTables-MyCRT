903b81d7847735c6b046ca92cb8a767bc8c63cc1
Hilary Schulz <hils124@yahoo.com>
2018-02-21 19:04:02 -0800

    Added ability to delete capture from the GUI

--- gui/src/pages/reducers/capture_reducers.ts
+++ gui/src/pages/reducers/capture_reducers.ts
@@ -1,42 +1,42 @@
-import { Action } from 'redux';
-import { AddCaptureAction, AddCaptureIdAction, AddGraphAction, ToggleGraphAction } from '../actions/capture_actions';
+// import { Action } from 'redux';
+// import { AddCaptureAction, AddCaptureIdAction, AddGraphAction, ToggleGraphAction } from '../actions/capture_actions';
 
-import { BrowserLogger as logger } from '../../logging';
+// import { BrowserLogger as logger } from '../../logging';
 
-export const toggleGraphReducer = (state: any, action: ToggleGraphAction) => {
-    return {
-        ...state,
-        show: action.value,
-    };
-};
+// export const toggleGraphReducer = (state: any, action: ToggleGraphAction) => {
+//     return {
+//         ...state,
+//         show: action.value,
+//     };
+// };
 
-let nextId = 0;
+// let nextId = 0;
 
-export const addGraphReducer = (state: any, action: AddGraphAction) => {
-    const id = nextId;
-    nextId += 1;
-    const result = {
-        ...state,
-        graphs: [...state.graphs, id],
-    };
-    result.graphsById[id] = {
-        show: action.show,
-        displayName: action.displayName,
-        data: action.data,
-    };
-    return result;
-};
+// export const addGraphReducer = (state: any, action: AddGraphAction) => {
+//     const id = nextId;
+//     nextId += 1;
+//     const result = {
+//         ...state,
+//         graphs: [...state.graphs, id],
+//     };
+//     result.graphsById[id] = {
+//         show: action.show,
+//         displayName: action.displayName,
+//         data: action.data,
+//     };
+//     return result;
+// };
 
-export const addCaptureIdReducer = (state: any, action: AddCaptureIdAction) => {
-    return {
-        ...state,
-        captureId: action.id,
-    };
-};
+// export const addCaptureIdReducer = (state: any, action: AddCaptureIdAction) => {
+//     return {
+//         ...state,
+//         captureId: action.id,
+//     };
+// };
 
-export const addCaptureReducer = (state: any, action: AddCaptureAction) => {
-    return {
-        ...state,
-        capture: action.capture,
-    };
-};
+// export const addCaptureReducer = (state: any, action: AddCaptureAction) => {
+//     return {
+//         ...state,
+//         capture: action.capture,
+//     };
+// };
--- gui/src/pages/states/capture_state.ts
+++ gui/src/pages/states/capture_state.ts
@@ -1,45 +1,45 @@
-import './common';
+// import './common';
 
-import '../../static/css/capture.css';
+// import '../../static/css/capture.css';
 
-import React = require('react');
-import ReactDom = require('react-dom');
-import { connect, Provider } from 'react-redux';
-import { Action, createStore, Store } from 'redux';
+// import React = require('react');
+// import ReactDom = require('react-dom');
+// import { connect, Provider } from 'react-redux';
+// import { Action, createStore, Store } from 'redux';
 
-import * as actions from '../actions/capture_actions';
-import * as reducers from '../reducers/capture_reducers';
+// import * as actions from '../actions/capture_actions';
+// import * as reducers from '../reducers/capture_reducers';
 
-const initialState = {
-    captureId: null,
-    capture: null,
-    replays: [],
-    replaysById: {},
-    graphs: [],
-    graphsById: {},
-};
+// const initialState = {
+//     captureId: null,
+//     capture: null,
+//     replays: [],
+//     replaysById: {},
+//     graphs: [],
+//     graphsById: {},
+// };
 
-const reducer = (state: any = initialState, action: Action) => {
-    switch (action.type) {
-        case actions.TOGGLE_GRAPH: {
-            const toggleAction = action as actions.ToggleGraphAction;
-            return reducers.toggleGraphReducer(state.graphsById[toggleAction.id], toggleAction);
-        }
-        case actions.ADD_GRAPH: {
-            const addAction = action as actions.AddGraphAction;
-            return reducers.addGraphReducer(state, addAction);
-        }
-        case actions.ADD_CAPTURE_ID: {
-            const addCaptureIdAction = action as actions.AddCaptureIdAction;
-            return reducers.addCaptureIdReducer(state, addCaptureIdAction);
-        }
-        case actions.ADD_CAPTURE: {
-            const addCaptureAction = action as actions.AddCaptureAction;
-            return reducers.addCaptureReducer(state, addCaptureAction);
-        }
-        default:
-          return state;
-      }
-};
+// const reducer = (state: any = initialState, action: Action) => {
+//     switch (action.type) {
+//         case actions.TOGGLE_GRAPH: {
+//             const toggleAction = action as actions.ToggleGraphAction;
+//             return reducers.toggleGraphReducer(state.graphsById[toggleAction.id], toggleAction);
+//         }
+//         case actions.ADD_GRAPH: {
+//             const addAction = action as actions.AddGraphAction;
+//             return reducers.addGraphReducer(state, addAction);
+//         }
+//         case actions.ADD_CAPTURE_ID: {
+//             const addCaptureIdAction = action as actions.AddCaptureIdAction;
+//             return reducers.addCaptureIdReducer(state, addCaptureIdAction);
+//         }
+//         case actions.ADD_CAPTURE: {
+//             const addCaptureAction = action as actions.AddCaptureAction;
+//             return reducers.addCaptureReducer(state, addCaptureAction);
+//         }
+//         default:
+//           return state;
+//       }
+// };
 
-const captureStore = createStore(reducer, initialState);
+// const captureStore = createStore(reducer, initialState);
