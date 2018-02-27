e5ead7b7dd8ece32f8e43aace8d5e6094a8e5d04
Hilary Schulz <hils124@yahoo.com>
2018-02-26 22:54:52 -0800

    Created generalized delete modal for environments and captures

--- gui/src/pages/components/delete_modal_comp.tsx
+++ gui/src/pages/components/delete_modal_comp.tsx
@@ -9,24 +9,24 @@ import { mycrt } from '../utils/mycrt-client';
 
 import { WarningAlert } from './alert_warning_comp';
 
-export class DeleteCaptureModal extends React.Component<any, any>  {
+export class DeleteModal extends React.Component<any, any>  {
 
     constructor(props: any) {
         super(props);
-        this.state = { captureName: "", deleteLogs: true };
+        this.state = { name: "", deleteLogs: true };
     }
 
     public async handleClick(event: any) {
-        if (this.state.captureName !== this.props.capture.name) {
+        if (this.state.name !== this.props.name) {
             logger.error("Given capture name doesn't match.");
             return;
         }
-        await mycrt.deleteCapture(this.props.capture.id, this.state.deleteLogs);
+        await this.props.delete(this.props.deleteId, this.state.deleteLogs);
         this.props.update();
     }
 
     public handleNameChange(event: any) {
-        this.setState({captureName: event.target.value});
+        this.setState({name: event.target.value});
     }
 
     public handleCheckChange(event: any) {
@@ -41,33 +41,33 @@ export class DeleteCaptureModal extends React.Component<any, any>  {
                 <div className="modal-dialog" role="document">
                     <div className="modal-content myCRT-modal">
                         <div className="modal-header myCRT-modal-danger">
-                            <h4 className="modal-title">Delete {this.props.capture.name}</h4>
+                            <h4 className="modal-title">Delete {this.props.name}</h4>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true" style={{color: "white"}}>&times;</span>
                             </button>
                         </div>
-                        <WarningAlert id="replayWarning" msg="Please fill in all the provided fields."
+                        <WarningAlert id="deleteWarning" msg="Please fill in all the provided fields."
                                       style = {{display: "none"}}/>
                         <div className="modal-body">
                             <form>
                                 <div className="form-group">
                                     <div className="card card-body bg-light">
                                         <label className="myCRT-danger-label"><b>
-                                            <i>Warning: Deleting this workload will also delete all associated
-                                            replay data. This action cannot be undone.</i></b></label>
+                                            <i>Warning: Deleting will also delete all associated data.
+                                                This action cannot be undone.</i></b></label>
                                         <br/>
-                                        <label><b>Type the Capture Name</b></label>
-                                        <input type="name" className="form-control" id="captureName"
-                                        value={this.state.captureName} onChange={this.handleNameChange.bind(this)}
-                                        aria-describedby="captureName" placeholder="Enter name"></input>
-                                            <small id="captureName" className="form-text text-muted"></small>
+                                        <label><b>Type the {this.props.type} Name</b></label>
+                                        <input type="name" className="form-control" id="deleteName"
+                                        value={this.state.name} onChange={this.handleNameChange.bind(this)}
+                                        aria-describedby="deleteName" placeholder="Enter name"></input>
+                                            <small id="deleteName" className="form-text text-muted"></small>
                                         <br/>
                                         <div className="form-check">
                                             <label className="form-check-label">
                                             <input type="checkbox" className="form-check-input"
                                                 onChange={(e) => this.handleCheckChange(e)}
                                                 defaultChecked={this.state.deleteLogs}/>
-                                            Delete Workload and Metrics Files
+                                            Delete All Workload and Metric Files
                                             </label>
                                         </div>
                                     </div>
@@ -75,10 +75,10 @@ export class DeleteCaptureModal extends React.Component<any, any>  {
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" id="cancelReplayBtn"
+                            <button type="button" className="btn btn-secondary" id="cancelButton"
                                     data-dismiss="modal">Cancel</button>
                             <button type="button" className="btn btn-danger"
-                                    onClick={this.handleClick.bind(this)}>Delete Capture</button>
+                                    onClick={this.handleClick.bind(this)}>Delete {this.props.type}</button>
                         </div>
                     </div>
                 </div>
