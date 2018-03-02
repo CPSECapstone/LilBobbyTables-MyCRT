5af7f6bc20e24220c30878a4a77ba0e9cbc79fdc
Nishanth <nishanth.dara@gmail.com>
2018-03-02 10:33:57 -0800

    Refactored validate api to use promises

--- service/src/routes/validate.ts
+++ service/src/routes/validate.ts
@@ -15,8 +15,9 @@ export default class ValidateRouter extends SelfAwareRouter {
    public name: string = 'validate';
    public urlPrefix: string = '/validate';
 
+   private logger = Logging.defaultLogger(__dirname);
+
    protected mountRoutes(): void {
-      const logger = Logging.defaultLogger(__dirname);
 
       this.router.get('/', this.handleHttpErrors(async (request, response) => {
          response.sendStatus(http.OK);
@@ -30,70 +31,99 @@ export default class ValidateRouter extends SelfAwareRouter {
             accessKeyId: request.body.accessKey,
             secretAccessKey: request.body.secretKey,
          });
-
          const params = {};
-         rds.describeDBInstances(params, (err, data) => {
-            if (err) {
-               response.sendStatus(400);
-            } else {
-               const instances: any = [];
-               if (data.DBInstances) {
-                  data.DBInstances.forEach((dbInstance: RDS.DBInstance) => {
-                     instances.push({
-                        instance: dbInstance.DBInstanceIdentifier,
-                        name: dbInstance.DBName,
-                        user: dbInstance.MasterUsername,
-                        host: dbInstance.Endpoint ? dbInstance.Endpoint.Address : "",
-                        parameterGroup: dbInstance.DBParameterGroups ?
-                           dbInstance.DBParameterGroups[0].DBParameterGroupName : "",
-                     });
-                  });
-               }
-               response.json(instances);
-            }
-         });
+
+         try {
+            const data = await this.getDBInstances(rds, params);
+            const instances: any = [];
+            data.DBInstances.forEach((dbInstance: RDS.DBInstance) => {
+               instances.push({
+                  instance: dbInstance.DBInstanceIdentifier,
+                  name: dbInstance.DBName,
+                  user: dbInstance.MasterUsername,
+                  host: dbInstance.Endpoint ? dbInstance.Endpoint.Address : "",
+                  parameterGroup: dbInstance.DBParameterGroups ?
+                     dbInstance.DBParameterGroups[0].DBParameterGroupName : "",
+               });
+            });
+            response.json(instances);
+         } catch (e) {
+            throw new HttpError(http.BAD_REQUEST, "Credentials are invalid");
+         }
       }));
 
       this.router.post('/database', check.validBody(schema.databaseBody),
          this.handleHttpErrors(async (request, response) => {
-            const connection = mysql.createConnection({
-               host : request.body.host,
-               user : request.body.user,
-               password : request.body.pass,
-               database : request.body.dbName,
-            });
+         const connection = mysql.createConnection({
+            host : request.body.host,
+            user : request.body.user,
+            password : request.body.pass,
+            database : request.body.dbName,
+         });
 
-            connection.connect((err) => {
-               if (err) {
-                  response.sendStatus(400);
-               } else {
-                  connection.destroy();
-                  response.sendStatus(http.OK);
-
-               }
-            });
+         try {
+            const conn = await this.getDBConnection(connection);
+            response.sendStatus(http.OK);
+         } catch (e) {
+            throw new HttpError(http.BAD_REQUEST, "Can't connect to the database");
+         }
       }));
 
       this.router.post('/bucket', check.validBody(schema.credentialsBody),
          this.handleHttpErrors(async (request, response) => {
-            const s3 = new S3({
-               region: request.body.region,
-               accessKeyId: request.body.accessKey,
-               secretAccessKey: request.body.secretKey,
-            });
+         const s3 = new S3({
+            region: request.body.region,
+            accessKeyId: request.body.accessKey,
+            secretAccessKey: request.body.secretKey,
+         });
+         const params = {};
 
-            const params = {};
-            s3.listBuckets((err, data) => {
-               if (err || !data.Buckets) {
-                  response.sendStatus(400);
-               } else {
-                  const buckets: any = [];
-                  data.Buckets.forEach((bucket: S3.Bucket) => {
-                     buckets.push(bucket.Name);
-                  });
-                  response.json(buckets);
-               }
+         try {
+            const data = await this.getBuckets(s3, params);
+            const buckets: any = [];
+            data.Buckets.forEach((bucket: S3.Bucket) => {
+               buckets.push(bucket.Name);
             });
+            response.json(buckets);
+         } catch (e) {
+            throw new HttpError(http.BAD_REQUEST, "Credentials are invalid");
+         }
       }));
+   }
+
+   private getDBInstances(rds: RDS, params: any): Promise<any> {
+      return new Promise((resolve, reject) => {
+         rds.describeDBInstances(params, (err, data) => {
+            if (err) {
+               reject(err);
+            } else {
+               resolve(data);
+            }
+         });
+      });
+   }
+
+   private getDBConnection(connection: mysql.Connection): Promise<any> {
+      return new Promise((resolve, reject) => {
+         connection.connect((err) => {
+            if (err) {
+               reject(err);
+            } else {
+               resolve(connection);
+            }
+         });
+      });
+   }
+
+   private getBuckets(s3: S3, params: any): Promise<any> {
+      return new Promise((resolve, reject) => {
+         s3.listBuckets((err, data) => {
+            if (err) {
+               reject(err);
+            } else {
+               resolve(data);
+            }
+         });
+      });
    }
 }
