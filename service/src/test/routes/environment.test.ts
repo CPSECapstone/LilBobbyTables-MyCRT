907555a249a1a6e71b46196f0c3f69422b11e426
Nishanth <nishanth.dara@gmail.com>
2018-03-07 16:23:57 -0800

    Added POST and PUT tests for environment/

--- service/src/test/routes/environment.test.ts
+++ service/src/test/routes/environment.test.ts
@@ -2,6 +2,7 @@ import { expect, request } from 'chai';
 import * as http from 'http-status-codes';
 import 'mocha';
 
+import { Logging } from '@lbt-mycrt/common/dist/main';
 import MyCrtService from '../../main';
 
 const newEnvBody = {
@@ -18,6 +19,49 @@ const newEnvBody = {
    parameterGroup: "supergroup",
 };
 
+const badEnvBody = {
+   accessKey: "ACCESSKEY",
+   secretKey: "SECRETKEY",
+   region: "somewhereineastasia",
+   bucket: "nfllogbucket",
+   dbName: "NFL",
+   host: "nfl2015.c7m7t1xyrt7v.us-east-2.rds.amazonaws.com",
+   user: "nfl2015user",
+   pass: "nfl2015pass",
+   envName: "NFL environment",
+   instance: "nfl2015",
+   parameterGroup: "supergroup",
+};
+
+const editEnvBody = {
+   accessKey: "ACCESSKEY",
+   secretKey: "SECRETKEY",
+   region: "us-east-2",
+   bucket: "nfllogbucket",
+   dbName: "NFL",
+   host: "nfl2015.c7m7t1xyrt7v.us-east-2.rds.amazonaws.com",
+   user: "nfl2015user",
+   pass: "nfl2015pass",
+   envName: "NFL sequel",
+   instance: "nfl2015",
+   parameterGroup: "supergroup",
+};
+
+const logger = Logging.defaultLogger(__dirname);
+
+function handleErrors(promise: Promise<any> | null) {
+   if (promise) {
+      return promise.catch((err) => {
+         if (err.code === 400) {
+            return err;
+         }
+         throw err;
+      });
+   } else {
+      return promise;
+   }
+ }
+
 export const environmentTests = (mycrt: MyCrtService) => () => {
 
    it("should post an environment", async () => {
@@ -26,4 +70,27 @@ export const environmentTests = (mycrt: MyCrtService) => () => {
       expect(response.body.id).to.equal(1);
    });
 
+   it("should reject the post of environment", async () => {
+      try {
+         const response = await request(mycrt.getServer()).post('/api/environments/').send(badEnvBody);
+      } catch (err) {
+         expect(err).to.have.status(http.BAD_REQUEST);
+      }
+   });
+
+   it("should edit an environment", async () => {
+      const responsePost = await request(mycrt.getServer()).post('/api/environments/').send(newEnvBody);
+      const response = await request(mycrt.getServer()).put('/api/environments/1').send(editEnvBody);
+      expect(response).to.have.status(http.OK);
+      expect(response.body.changedRows).to.equal(1);
+   });
+
+   it("should reject the edit of environment", async () => {
+      try {
+         const responsePost = await request(mycrt.getServer()).post('/api/environments/').send(newEnvBody);
+         const response = await handleErrors(request(mycrt.getServer()).put('/api/environments/1').send(badEnvBody));
+      } catch (err) {
+         expect(err).to.have.status(http.BAD_REQUEST);
+      }
+   });
 };
