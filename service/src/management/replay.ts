4098f7ee936bd5f0e1152f4c2413d42ae23de832
Cameron Taylor <axis7818@gmail.com>
2018-04-17 15:27:04 -0700

    mark replays as failed during startup if they are in an active state

--- service/src/management/replay.ts
+++ service/src/management/replay.ts
@@ -1,4 +1,21 @@
+import { ChildProgramStatus, Logging } from '@lbt-mycrt/common';
 
-export function markAbandonedReplaysAsFailed() {
+import { replayDao } from '../dao/mycrt-dao';
 
+const logger = Logging.defaultLogger(__dirname);
+
+export async function markAbandonedReplaysAsFailed() {
+   logger.info(`Looking for abandoned replays`);
+
+   const abandonedReplays = await replayDao.getAbandonedReplays();
+   if (abandonedReplays) {
+      logger.info(`Found ${abandonedReplays.length} abandoned replays.` +
+         `Marking them as failed.`);
+      for (const replay of abandonedReplays) {
+         logger.info(`Marking replay ${replay.id} as ${ChildProgramStatus.FAILED}`);
+         await replayDao.updateReplayStatus(replay.id!, ChildProgramStatus.FAILED);
+      }
+   } else {
+      logger.error(`Failed to get abandoned replays`);
+   }
 }
