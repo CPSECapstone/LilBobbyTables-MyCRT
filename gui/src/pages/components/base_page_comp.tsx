3a917f7c7a63e68324b73a441d1f7e2f5439c3e0
Cameron Taylor <axis7818@gmail.com>
2018-05-06 11:37:17 -0700

    generic error handling

--- gui/src/pages/components/base_page_comp.tsx
+++ gui/src/pages/components/base_page_comp.tsx
@@ -1,9 +1,12 @@
+import '../utils/errors';
+
 import React = require('react');
 import ReactDom = require('react-dom');
 import { Provider } from 'react-redux';
 
 import * as $ from 'jquery';
 
+import { showAlert } from '../../actions';
 import { Alert } from '../../components/alert';
 import { BrowserLogger as logger } from '../../logging';
 import { store } from '../../store';
@@ -26,6 +29,16 @@ export class BasePage extends React.Component<any, State> {
    public async componentDidMount() {
       const user = await mycrt.aboutMe();
       this.setState({ loggedIn: !user });
+   }
+
+   public componentDidCatch(error: Error, info: React.ErrorInfo) {
+      logger.error(error.message);
+      logger.error(info.componentStack);
+      store.dispatch(showAlert({
+         show: true,
+         header: "An Unexpected Error Occurred",
+         message: error.message,
+      }));
    }
 
    public render() {
--- gui/src/pages/utils/errors.ts
+++ gui/src/pages/utils/errors.ts
@@ -1,8 +1,30 @@
+import { showAlert } from '../../actions';
 import { BrowserLogger as logger } from '../../logging';
+import { store } from '../../store';
 
-function genericErrorHandler(event: Event) {
-   logger.error("An Error Occurred");
-   logger.error(JSON.stringify(event));
-}
+window.onerror = function(eventOrMsg: any, source?: string, lineNo?: number, columnNo?: number,
+      error?: any) {
 
-window.addEventListener('error', genericErrorHandler, true);
+   const str = typeof eventOrMsg === 'string' ? eventOrMsg.toLowerCase() : "";
+   const substr = "script error";
+   let msg: string = "";
+   if (str.indexOf(substr) > -1) {
+      msg = "Script Error: See Browser Console for Detail";
+   } else {
+      msg = [
+         "Message: " + JSON.stringify(eventOrMsg),
+         "URL: " + source,
+         "Line: " + lineNo,
+         "Column: " + columnNo,
+         "Error object: " + JSON.stringify(error),
+      ].join(' - ');
+   }
+
+   store.dispatch(showAlert({
+      show: true,
+      header: "An Unexpected Error Has Occurred",
+      message: msg,
+   }));
+
+   return false;
+};
