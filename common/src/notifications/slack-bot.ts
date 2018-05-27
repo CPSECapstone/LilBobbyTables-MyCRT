c8c5ffcbf913950ae6f81d3c7694f6d43e1bbf94
Nishanth <nishanth.dara@gmail.com>
2018-05-27 14:37:07 -0700

    Abstracted bot token to be pulled from database

--- common/src/notifications/slack-bot.ts
+++ common/src/notifications/slack-bot.ts
@@ -1,5 +1,7 @@
 import { WebClient } from '@slack/client';
+import * as mysql from 'mysql';
 
+import { mycrtDbConfig } from '../dao/config';
 import { defaultLogger } from '../logging';
 
 const logger = defaultLogger(__dirname);
@@ -11,7 +13,9 @@ const channelId = 'C7A678PRV';
 const web = new WebClient(token);
 
 export class SlackBot {
-   public static async postMessage(message: string): Promise<any> {
+   public static async postMessage(message: string, envId: number): Promise<any> {
+      const shit = await this.getSlackConfig(envId);
+
       web.chat.postMessage({
          channel: channelId,
          text: message,
@@ -21,6 +25,29 @@ export class SlackBot {
       })
       .catch(() => {
          logger.error("Message failed to send to slack");
+      });
+   }
+
+   private static async getSlackConfig(envId: number): Promise<any> {
+      const query = 'SELECT token, channel FROM Slackbot WHERE id = ?';
+      const conn = await mysql.createConnection(mycrtDbConfig);
+      return new Promise((resolve, reject) => {
+         conn.connect((connErr, myConn) => {
+            if (connErr) {
+               logger.error(`Error getting connection: ${connErr}`);
+               reject(connErr);
+            } else {
+               myConn.query(query, (queryErr: any, results: any, fields: any) => {
+                  myConn.close();
+                  if (queryErr) {
+                     logger.error(`Error making query: ${queryErr}`);
+                     reject(queryErr);
+                  } else {
+                     resolve(results);
+                  }
+               });
+            }
+         });
       });
    }
 }
