dbe4cabf8a844eecb9412e8af0aae5e0bd011394
Nishanth <nishanth.dara@gmail.com>
2018-05-29 21:54:23 -0700

    Fixed a bug with slackbot and added a message when slack is integrated

--- common/src/notifications/slack-bot.ts
+++ common/src/notifications/slack-bot.ts
@@ -15,6 +15,7 @@ const web = new WebClient(token);
 export class SlackBot {
    public static async postMessage(message: string, envId: number): Promise<any> {
       const slackConfig = await this.getSlackConfig(envId);
+      logger.debug(JSON.stringify(slackConfig));
       if (slackConfig.length !== 0 && slackConfig[0].isOn) {
          web.chat.postMessage({
             channel: slackConfig[0].channel,
@@ -30,7 +31,7 @@ export class SlackBot {
    }
 
    private static async getSlackConfig(envId: number): Promise<any> {
-      const query = mysql.format('SELECT token, channel FROM SlackConfig WHERE environmentId = ?', [envId]);
+      const query = mysql.format('SELECT token, channel, isOn FROM SlackConfig WHERE environmentId = ?', [envId]);
       const conn = await mysql.createConnection(mycrtDbConfig);
       return new Promise((resolve, reject) => {
          conn.connect((connErr, myConn) => {
