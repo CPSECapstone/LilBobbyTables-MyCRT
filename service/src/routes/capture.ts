eb89dc09a40c34905761952828a7323c6d606163
Hilary Schulz <hils124@yahoo.com>
2018-05-31 09:10:32 -0700

    Fixed delete bug for captures and replays

--- service/src/routes/capture.ts
+++ service/src/routes/capture.ts
@@ -398,7 +398,7 @@ export default class CaptureRouter extends SelfAwareRouter {
                await storage.deletePrefix(capturePrefix);
             }
          }
-         response.status(http.OK).end();
+         response.json(captureDel);
       }));
    }
 
